39fc73268ebf9daa47e4de52e50ef4b940643bd3
suffocation9834 <149144424+suffocation9834@users.noreply.github.com>
2023-11-17 23:20:20 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-document.getElementById("nextPageIcon").addEventListener("click", function() {
-    window.location.href = 'nextpage.html'; // Redirect to the next page
-});
+var video = document.getElementById("background-video");
+
+window.onload = function() {
+    var button = document.getElementById("muteUnmuteButton");
+    button.textContent = video.muted ? '🔊' : '🔇';
+};
+
+function toggleMuteUnmute() {
+    var button = document.getElementById("muteUnmuteButton");
+    if (video.muted) {
+        video.muted = false;
+        button.textContent = '🔇';
+    } else {
+        video.muted = true;
+        button.textContent = '🔊';
+    }
+}
+
+function adjustVolume(volume) {
+    video.volume = volume;
+}
